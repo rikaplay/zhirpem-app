14f78c2bccdd80d6644153f9f4d73fe2526a5dff
rikaplay <richikcat51@gmail.com>
2026-07-22 21:03:57 +0300

    Fix build configuration for Vercel

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,7 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlinComposePlugin) apply false
+    alias(libs.plugins.kotlinMultiplatform) apply false
+    alias(libs.plugins.composeMultiplatform) apply false
     id("com.google.gms.google-services") version "4.4.1" apply false
 }
